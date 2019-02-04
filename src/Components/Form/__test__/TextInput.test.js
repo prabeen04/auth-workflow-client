ee268db07d1d5f893b565a6eafc623a9dd2154af
Prabeen04 <prabeen.strange@gmail.com>
2019-02-04 20:08:47 +0530

    upload test boilerplate

---
+++ src/Components/Form/__test__/TextInput.test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render, fireEvent } from 'react-testing-library'
+import TextInput from '../TextInput';
+
+test('should render input  ', () => {
+    console.log('test')
+})
