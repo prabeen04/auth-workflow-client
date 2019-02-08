c7d51e07d9da53918011c5eb0c9b63bd5b605df7
Prabeen04 <prabeen.strange@gmail.com>
2019-02-08 20:25:11 +0530

    ViewEditCard implementation changed  to hooks

--- src/Components/Utils/ViewEditCard.js
+++ src/Components/Utils/ViewEditCard.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+function ViewEditCard() {
+    const [viewType, setViewType] = useState('view')
+    function toggleViewType() {
+        viewType === 'view'
+            ? setViewType('edit')
+            : setViewType('view')
+    }
 
-class ViewEditCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            viewType: 'view',
-        };
-    }
-    toggleViewType = () => {
-        this.setState(prevState => {
-            if (prevState.viewType === 'view') {
-                return { viewType: 'edit' }
-            } else {
-                return { viewType: 'view' }
-            }
-        })
-    }
-    render() {
-        return (
-            <>
-                {this.props.children(this.state, this.toggleViewType )}
-            </>
-        );
-    }
+    return { viewType, toggleViewType }
 }
-
 export default ViewEditCard;
+// class ViewEditCard extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             viewType: 'view',
+//         };
+//     }
+//     toggleViewType = () => {
+//         this.setState(prevState => {
+//             if (prevState.viewType === 'view') {
+//                 return { viewType: 'edit' }
+//             } else {
+//                 return { viewType: 'view' }
+//             }
+//         })
+//     }
+//     render() {
+//         return (
+//             <>
+//                 {this.props.children(this.state, this.toggleViewType )}
+//             </>
+//         );
+//     }
+// }
+
+// export default ViewEditCard;
--- src/Containers/Profile/Profile.js
+++ src/Containers/Profile/Profile.js
@@ -26,6 +26,7 @@ export class Profile extends Component {
         <img src={avatarUrl} alt="" style={{ width: 100, height: 100, borderRadius: '50%' }} />
         <h3>{`Welcome ${userName}`}</h3>
         <h2>{email}</h2>
+        
         <ViewEditCard>
           {({ viewType }, toggleViewType) => (
             viewType === 'view'
@@ -34,13 +35,13 @@ export class Profile extends Component {
           )}
         </ViewEditCard>
         <br />
-        <ViewEditCard>
+        {/* <ViewEditCard>
           {({ viewType }, toggleViewType) => (
             viewType === 'view'
               ? <a onClick={() => toggleViewType()}>Change password</a>
               : <ChangePassword toggleViewType={toggleViewType} />
           )}
-        </ViewEditCard>
+        </ViewEditCard> */}
         <br/>
         <Button type='danger' onClick={this.handleLogout}>Logout</Button>
       </>
