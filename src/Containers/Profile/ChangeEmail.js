9cb2062714517dbd5f8fc5ec7c9757c445394b56
Prabeen04 <prabeen.strange@gmail.com>
2019-02-09 21:45:46 +0530

    ChangeEmail component refactored to using hooks

--- src/Containers/Profile/ChangeEmail.js
+++ src/Containers/Profile/ChangeEmail.js
@@ -1,59 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Button, message } from "antd";
 import TextInput from "../../Components/Form/TextInput";
 import { changeEmail } from "../Auth/AuthAction";
 
-class ChangeEmail extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            newEmail: '',
-        }
-    }
-    handleChange = ({ target: { name, value } }) => this.setState({ [name]: value })
-    changeEmailCallBack = (status) => {
-        this.setState({ newEmail: '' })
+function ChangeEmail(props) {
+    const [newEmail, setNewEmail] = useState('')
+    const changeEmailCallBack = (status) => {
+        setNewEmail('')
         status === 'success'
             ? message.success('email sent for verification')
             : message.error('error changing email !')
-        this.props.toggleViewType()
+        props.toggleViewType()
     }
-    handleChangeEmail = () => {
-        const { _id } = this.props.user;
-        const { newEmail } = this.state;
-        this.props.changeEmail({ _id, email: newEmail }, this.changeEmailCallBack)
+    const handleChangeEmail = () => {
+        const { _id } = props.user;
+        props.changeEmail({ _id, email: newEmail }, changeEmailCallBack)
     }
-    render() {
-        const { newEmail } = this.state;
-        const { toggleViewType, sendingEmail } = this.props
-        return (
-            <>
-                <TextInput
-                    isRequired
-                    type='email'
-                    name='newEmail'
-                    value={newEmail}
-                    label='Enter new email'
-                    onChange={this.handleChange}
-                    placeholder='example@example.com'
-                />
-                <Button
-                    type='primary'
-                    loading={sendingEmail}
-                    disabled={!newEmail}
-                    onClick={this.handleChangeEmail}
-                    icon='login'>
-                    Change Email</Button>
-                <Button
-                    type='danger'
-                    onClick={toggleViewType}>
-                    Cancel</Button>
-            </>
-
-        )
-    }
+    return (
+        <>
+            <TextInput
+                isRequired
+                type='email'
+                name='newEmail'
+                value={newEmail}
+                label='Enter new email'
+                onChange={({ target: { value } }) => setNewEmail(value)}
+                placeholder='example@example.com'
+            />
+            <Button
+                type='primary'
+                loading={props.sendingEmail}
+                disabled={!newEmail}
+                onClick={handleChangeEmail}
+                icon='login'>
+                Change Email</Button>
+            <Button
+                type='danger'
+                onClick={props.toggleViewType}>
+                Cancel</Button>
+        </>
+    )
 }
 
 const mapStateToProps = ({ auth }) => ({
