f8e95649fa4d768c82f92f5c58a2d34cbab4a66f
Prabeen04 <prabeen.strange@gmail.com>
2019-02-11 20:29:34 +0530

    small stuff

--- src/Containers/Profile/Profile.js
+++ src/Containers/Profile/Profile.js
@@ -27,12 +27,12 @@ function Profile(props) {
         <Menu
           theme="dark"
           mode="horizontal"
-          defaultSelectedKeys={['2']}
+          defaultSelectedKeys={['3']}
           style={{ lineHeight: '64px' }}
         >
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu.Item key="1">Activity</Menu.Item>
+          <Menu.Item key="2">Images</Menu.Item>
+          <Menu.Item key="3">Profile</Menu.Item>
         </Menu>
       </Header>
       <Content>
@@ -48,9 +48,6 @@ function Profile(props) {
           </>
         </div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>
-        Ant Design ©2018 Created by Ant UED
-    </Footer>
     </Layout>
 
   )
