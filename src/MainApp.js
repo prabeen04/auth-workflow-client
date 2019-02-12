45460a8a788fe7651b851c1d9809d6561ed81022
Prabeen04 <prabeen.strange@gmail.com>
2019-02-12 21:21:57 +0530

    Image and Activity route added

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,8 +1,10 @@
 import React, { lazy, Suspense } from 'react'
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Menu, } from 'antd';
-const Profile = lazy(() => import('./Containers/Profile/Profile'))
 const { Header, Content, Footer } = Layout;
+const Activity = lazy(() => import('./Containers/Activity/Activity'))
+const Image = lazy(() => import('./Containers/Image/Image'))
+const Profile = lazy(() => import('./Containers/Profile/Profile'))
 
 function MainApp(props) {
     return (
@@ -18,13 +20,15 @@ function MainApp(props) {
                     style={{ lineHeight: '64px' }}
                 >
                     <Menu.Item key="1"><Link to='/'>Activity</Link></Menu.Item>
-                    <Menu.Item key="2"><Link to='/images'>Images</Link></Menu.Item>
+                    <Menu.Item key="2"><Link to='/image'>Images</Link></Menu.Item>
                     <Menu.Item key="3"><Link to='/profile'>Profile</Link></Menu.Item>
                 </Menu>
             </Header>
                     <Content>
                         <Suspense fallback='loading ...'>
                             <Switch>
+                                <Route exact path='/' component={Activity} />
+                                <Route exact path='/image' component={Image} />
                                 <Route exact path='/profile' component={Profile} />
                             </Switch>
                         </Suspense>
