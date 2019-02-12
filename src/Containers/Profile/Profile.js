06283aa2a36a7065c399f674cdb153531e9752a7
Prabeen04 <prabeen.strange@gmail.com>
2019-02-12 21:10:15 +0530

    Profile Code cleaned

--- src/Containers/Profile/Profile.js
+++ src/Containers/Profile/Profile.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from "redux";
 import { withRouter, Link } from "react-router-dom";
-import { Layout, Menu, Breadcrumb, Button, message } from 'antd';
+import { Button, message } from 'antd';
 import { logout } from "../Auth/AuthAction";
 import { base_url } from "../../Config/config";
 import { ViewEditCard } from "../../Components/Utils";
 import ChangePassword from './ChangePassword';
 import ChangeEmail from './ChangeEmail';
-const { Header, Content, Footer } = Layout;
 
 function Profile(props) {
   const { user: { email, userName, avatar } } = props;
@@ -21,35 +20,19 @@ function Profile(props) {
     props.history.push('/login')
   }
   return (
-    <Layout className="layout">
-      <Header>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['3']}
-          style={{ lineHeight: '64px' }}
-        >
-          <Menu.Item key="1">Activity</Menu.Item>
-          <Menu.Item key="2">Images</Menu.Item>
-          <Menu.Item key="3">Profile</Menu.Item>
-        </Menu>
-      </Header>
-      <Content>
-        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-          <>
-            <img src={avatarUrl} alt="" style={{ width: 100, height: 100, borderRadius: '50%' }} />
-            <h3>{`Welcome ${userName}`}</h3>
-            <h2>{email}</h2>
-            <ChangeEmail /><br />
-            <ChangePassword />
-            <br />
-            <Button type='danger' onClick={handleLogout}>Logout</Button>
-          </>
-        </div>
-      </Content>
-    </Layout>
-
+    <>
+      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+        <>
+          <img src={avatarUrl} alt="" style={{ width: 100, height: 100, borderRadius: '50%' }} />
+          <h3>{`Welcome ${userName}`}</h3>
+          <h2>{email}</h2>
+          <ChangeEmail /><br />
+          <ChangePassword />
+          <br />
+          <Button type='danger' onClick={handleLogout}>Logout</Button>
+        </>
+      </div>
+    </>
   )
 }
 const mapStateToProps = ({ auth }) => ({
