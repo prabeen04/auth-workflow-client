78a8ceb4447596fb0a068a6f00fc6fac800f0990
Prabeen04 <prabeen.strange@gmail.com>
2019-02-12 21:20:02 +0530

    code cleaned

--- src/Components/Utils/ViewEditCard.js
+++ src/Components/Utils/ViewEditCard.js
@@ -13,29 +13,4 @@ function ViewEditCard() {
     return { viewType, toggleViewType }
 }
 
-// class ViewEditCard extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             viewType: 'view',
-//         };
-//     }
-// toggleViewType = () => {
-//     this.setState(prevState => {
-//         if (prevState.viewType === 'view') {
-//             return { viewType: 'edit' }
-//         } else {
-//             return { viewType: 'view' }
-//         }
-//     })
-// }
-//     render() {
-//         return (
-//             <>
-//                 {this.props.children(this.state, this.toggleViewType )}
-//             </>
-//         );
-//     }
-// }
-
 export default ViewEditCard;
