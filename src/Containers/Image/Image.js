ee2646a790ac97ee5141fad2de9db2e439082f2e
Prabeen04 <prabeen.strange@gmail.com>
2019-02-13 20:31:32 +0530

    images states and data set

--- src/Containers/Image/Image.js
+++ src/Containers/Image/Image.js
@@ -3,15 +3,20 @@ import { PIXABAY_URL, PIXABAY_KEY } from "../../Config/config";
 import axios from 'axios';
 
 export default function Image(props) {
-  const [images, setImages] = useState([]);
+  const [imageState, setImages] = useState({ isFetching: false, images: [] });
   useEffect(() => {
+    setImages({ ...imageState, isFetching: true })
     axios.get(`${PIXABAY_URL}${PIXABAY_KEY}&q=react`)
-      .then(res => console.log(res))
+      .then(res => {
+        console.log(res)
+        setImages({ ...imageState, isFetching: false, images: res.data.hits })
+      })
       .catch(err => console.log(err))
   })
   return (
     <>
       <h2>image component</h2>
+      {imageState.isFetching && <p>fetching images ...</p>}
     </>
   )
 }
