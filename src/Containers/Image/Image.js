084f4a1aacd2a37837eeea27fa92f606323aaaaf
Prabeen04 <prabeen.strange@gmail.com>
2019-02-14 23:29:09 +0530

    image rendered via ProgressiveImage component

--- src/Containers/Image/Image.js
+++ src/Containers/Image/Image.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { PIXABAY_URL, PIXABAY_KEY } from "../../Config/config";
 import axios from 'axios';
-
+import { ProgressiveImage } from "../../Components/Utils";
 export default function Image(props) {
   const [imageState, setImages] = useState({ isFetching: false, images: [] });
   useEffect(() => {
@@ -18,7 +18,13 @@ export default function Image(props) {
       <h2>image component</h2>
       {imageState.isFetching && <p>fetching images ...</p>}
       {imageState.images && imageState.images.map(image => {
-        return <img src={image.previewURL} height={100} width={100}/>
+        // return <img src={image.previewURL} height={100} width={100}/>
+        return (
+          <ProgressiveImage
+            preview={image.previewURL}
+            image={image.largeImageURL}
+          />
+        )
       })}
     </>
   )
