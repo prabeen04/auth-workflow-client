f2507e3e7ea6e2dcd956dbefc73bb7a79876a9f5
Prabeen04 <prabeen.strange@gmail.com>
2019-02-15 21:34:50 +0530

    Context removed

--- src/Containers/Image/Image.js
+++ src/Containers/Image/Image.js
@@ -3,10 +3,9 @@ import { PIXABAY_URL, PIXABAY_KEY } from "../../Config/config";
 import axios from 'axios';
 import { ProgressiveImage } from "../../Components/Utils";
 import ImageForm from './ImageForm';
-import { QueryContext } from "./ImageForm";
 export default function Image(props) {
   const [imageState, setImages] = useState({ isFetching: false, images: [] });
-  const { query } = useContext(QueryContext)
+  console.log(query)
   useEffect(() => {
     setImages({ ...imageState, isFetching: true })
     axios.get(`${PIXABAY_URL}${PIXABAY_KEY}&q=react`)
@@ -19,10 +18,6 @@ export default function Image(props) {
   return (
     <>
       <ImageForm />
-      <QueryContext.Consumer>
-        {(value) => <p>{value}</p>}
-      </QueryContext.Consumer>
-      <p>{query}</p>
       {imageState.isFetching && <p>fetching images ...</p>}
       {imageState.images && imageState.images.map((image, i) => {
         // return <img src={image.previewURL} height={100} width={100}/>
--- src/Containers/Image/ImageForm.js
+++ src/Containers/Image/ImageForm.js
@@ -1,22 +1,19 @@
-import React, { useState, createContext } from 'react'
+import React, { useState } from 'react'
 import { Button } from "antd";
 import { TextInput } from "../../Components/Form";
-export const QueryContext = createContext('')
 
 export default function ImageForm() {
     const [query, setQuery] = useState('')
     return (
         <>
-            <QueryContext.Provider value={query}>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <TextInput
-                        onChange={({ target: { value } }) => setQuery(value)}
-                        value={query}
-                        placeholder='Search image ...'
-                    />
-                    <Button type='primary'>Search</Button>
-                </div>
-            </QueryContext.Provider>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <TextInput
+                    onChange={({ target: { value } }) => setQuery(value)}
+                    value={query}
+                    placeholder='Search image ...'
+                />
+                <Button type='primary'>Search</Button>
+            </div>
         </>
     )
 }
